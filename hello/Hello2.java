5b55046677bda41cbc0e2eb983defaf1c997956d
karlissonJR <karlissonj@gmail.com>
2016-05-01 09:52:37 -0300

    kommit

--- hello/Hello2.java
+++ hello/Hello2.java
@@ -1,7 +1,7 @@
 public class Hello2{
 
 	public static void main(String[] args){
-		System.out.print("Hello ");
-		System.out.println("World!");
+		System.out.print("Hello");
+		System.out.println(" World!");
 	}
 }
